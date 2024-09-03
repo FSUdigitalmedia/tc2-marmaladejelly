e81d19c4670e88ba6fb2b1e0223399d0e85cdead
Amanda Anderson <ada23a@fsu.edu>
2024-09-03 09:46:23 -0400

    drawing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
 function setup() {
+  //creates a canvas 600 pixels wide
+  //and 400 pixels high
   createCanvas(600, 400);
 }
 
 function draw() {
-  background(125);
+  //sky blue background
+  background(135, 206, 235);
+  // sun in top-right corner
+  fill("yellow"); //yellow
+
+  stroke("orange"); //orange outline
+
+  strokeWeight(20); //large outline
+
+  circle(550, 50, 100)
+  //grass on bottom half
+
+  stroke(0);//black outline
+
+  strokeWeight(1);//outline thickness
+
+  fill("green")
+
+  rect(0, 200, 600, 200)
+
+  //emojis
+  textSize(75)
+  text("🌸", 100, 250) //flower
+  text("🐞", 300, 250) //ladybug
+  
 }
